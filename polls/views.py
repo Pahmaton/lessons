29fcd3fd2680630b29440c9dfa15e5438b69d95a
pc <truhlyaev24@gmail.com>
2020-06-23 21:54:29 +0300

    final

--- polls/views.py
+++ polls/views.py
@@ -27,10 +27,17 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
+        # asd = 1
+        # f'asd{asd}'
+        # 'asd_' + asd
+        # 'asd_%s' % asd
+        # 'asd_{a}'.format(a=asd)
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
             'question': question,
